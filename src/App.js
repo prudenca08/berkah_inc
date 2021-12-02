0ff969d609039d2c8a39e640e22526b1e64b5584
prudenca08 <prudencaahdaffa08@gmail.com>
2021-12-03 05:32:34 +0700

    FRONTEND

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
-import Login from "./components/Login/Login";
-import Daftar from "./components/Daftar/Daftar";
-
+import LoginPage from "./pages/LoginPage";
+import DaftarPage from "./pages/DaftarPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -11,8 +10,8 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/daftar" component={Daftar} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/daftar" component={DaftarPage} />
+          <Route exact path="/login" component={LoginPage} />
           <Route exact path="/" component={HomePage} />
           <Route exact path="/projects/:id" component={ProjectsPage} />
         </Switch>
---
+++ src/pages/DaftarPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Daftar from "../components/Daftar/Daftar";
+
+export default function DaftarPage() {
+  return (
+    <div>
+      <Daftar />
+    </div>
+  );
+}
